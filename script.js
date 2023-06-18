450380d7c299d430e27ec9f09344a8f0168cc0af
Anthony <origamifortunes@gmail.com>
2023-06-18 00:23:08 -0400

    updated gallery img to fade in on scroll

---
+++ script.js
@@ -0,0 +1,22 @@
+window.addEventListener("scroll", function() {
+    var fadeContainers = document.querySelectorAll(".fade-container");
+    var screenPosition;
+  
+    // Adjust screenPosition based on device screen size
+    if (window.innerWidth < 768) {
+      // For screens smaller than 768px (mobile devices)
+      screenPosition = window.innerHeight / 1.1;
+    } else {
+      // For larger screens
+      screenPosition = window.innerHeight / 1.3;
+    }
+  
+    fadeContainers.forEach(function(container) {
+      var fadeContainerPosition = container.getBoundingClientRect().top;
+  
+      if (fadeContainerPosition < screenPosition) {
+        container.classList.add("fade-in");
+      }
+    });
+  });
+  
